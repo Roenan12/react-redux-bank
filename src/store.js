b1ef9ba7ecfbe400a30c53f7d8a5e4a1ed5bef55
Roenan Halili <haliliroenan12@gmail.com>
2024-09-26 16:58:13 +0800

    refactor: implement  redux-logger middleware in redux store

--- src/store.js
+++ src/store.js
@@ -1,12 +1,17 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, compose, combineReducers, createStore } from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
+import { logger } from "redux-logger/src";
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+// listens to every action and logs into console (middleware)
+const middlewares = [logger];
+const composedEnhancers = compose(applyMiddleware(...middlewares));
+
+const store = createStore(rootReducer, undefined, composedEnhancers);
 
 export default store;
